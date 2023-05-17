c9d9d7df2064a7b90644821531ca8ac3d3d0d57a
Leteinturier Alix <al.leteinturier@gmail.com>
2023-05-17 17:59:04 +0200

    Ajout d'un compteur dans le dropdown + modifs css

--- src/components/ButtonS.js
+++ src/components/ButtonS.js
@@ -7,24 +7,60 @@ import '../styles/ButtonS.css'
 function ButtonS() {
 
     const [open, setOpen] = React.useState(false);
-
     const handleOpen = () => {
       setOpen(!open);
     }
 
     const[rotateIcon, setRotateIcon]  = React.useState(false);
-
     const handleRotate = () => {
       setRotateIcon(!rotateIcon);
     }
-
     const rotate = rotateIcon ? "rotate(180deg)" : "rotate(0)"
-    
+
+    const [countA, setCountA] = React.useState(0);
+    const handlePlusA = () => {
+      if (countT<9){
+        setCountA(countA + 1);
+      }
+    }
+    const handleMoinsA = () => {
+      if ((countA-1)>=0){
+        setCountA(countA - 1);
+      }
+    }
+
+    const [countY, setCountY] = React.useState(0);
+    const handlePlusY = () => {
+      if (countT<9){
+        setCountY(countY + 1);
+      }
+    }
+    const handleMoinsY = () => {
+      if ((countY-1) >= 0){
+        setCountY(countY - 1);
+      }
+      
+    }
+
+    const [countS, setCountS] = React.useState(0);
+    const handlePlusS = () => {
+      if (countT<9){
+        setCountS(countS + 1);
+      }
+    }
+    const handleMoinsS = () => {
+      if ((countS-1) >= 0){
+        setCountS(countS - 1);
+      }
+    }
+
+    const countT = countA + countY + countS;
+
 
     return (
       <div className="dropdowns">
         <button className="btns" onClick ={() => {handleOpen(); handleRotate()}}>
-          <div className='text'>choice entre crochet</div>
+          <div className='text'>{countT} Passangers</div>
           <div className='imgs'>
             <img src={down} style={{ transform: rotate, transition: "all 0.2s linear" }} className='icon-up-down' ></img>
           </div>
@@ -34,31 +70,55 @@ function ButtonS() {
               <ul className="list">
                 <li>
                   <div className='row'>
-                    <span className='left'>Adults</span>
+                    <div className='left'>
+                      <span className='cat'>Adults</span>
+                      <span className='age'>26+ years </span>
+                    </div>
                     <div className='add'>
                       {/*<button onClick ={() => setChoice("One-way")} className=" center"  >moins</button>*/}
-                      <button className="btn-item center"><img src={minus} alt="icon moins" /></button>
-                      <button className="btn-item right"><img src={plus} alt="icon plus" /></button>
+                      <button id="moinsA" onClick= {handleMoinsA} className="btn-item">
+                        <img src={minus} alt="icon moins" />
+                      </button>
+                      <div className='conter'>{countA}</div>
+                      <button id="plusA" onClick= {handlePlusA} className="btn-item">
+                        <img src={plus} alt="icon plus" />
+                      </button>
                     </div>
                   </div>
                 </li>
                 <li>
                   <div className='row'>
-                    <span className='left'>Youth</span>
+                    <div className='left'>
+                      <span className='cat'>Youth</span>
+                      <span className='age'>0-25 years </span>
+                    </div>
                     <div className='add'>
                       {/*<button onClick ={() => setChoice("One-way")} className=" center"  >moins</button>*/}
-                      <button className="btn-item center"><img src={minus} alt="moins" /></button>
-                      <button className="btn-item right"><img src={plus} alt="icon plus"/></button>
+                      <button id="moinsY" onClick= {handleMoinsY} className="btn-item">
+                        <img src={minus} alt="icon moins" />
+                      </button>
+                      <div className='conter'>{countY}</div>
+                      <button id="plusY" onClick= {handlePlusY} className="btn-item">
+                        <img src={plus} alt="icon plus"/>
+                      </button>
                     </div>
                   </div>
                 </li>
                 <li>
                   <div className='row'>
-                    <span className='left'>Seniors</span>
+                    <div className='left'>
+                      <span className='cat'>Senior</span>
+                      <span className='age'>58+ years </span>
+                    </div>
                     <div className='add'>
                       {/*<button onClick ={() => setChoice("One-way")} className=" center"  >moins</button>*/}
-                      <button className="btn-item center"><img src={minus} alt="moins" /></button>
-                      <button className="btn-item right"><img src={plus} alt="icon plus"/></button>
+                      <button id="moinsS" onClick= {handleMoinsS} className="btn-item">
+                        <img src={minus} alt="icon moins" />
+                      </button>
+                      <div className='conter'>{countS}</div>
+                      <button id="plusS" onClick= {handlePlusS} className="btn-item">
+                        <img src={plus} alt="icon plus"/>
+                      </button>
                     </div>
                   </div>
                 </li>
